472a78a69dfc845865bf43a04128b8044fb98536
James <140310844+ChanChun9301@users.noreply.github.com>
2025-12-23 13:07:38 +0500

    '231225'

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import CustomUser, Group, TeamTable
+from .serializers import UserSerializer, GroupSerializer, TeamTableSerializer
 
-# Create your views here.
+# CRUD для пользователей
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# CRUD для групп
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# CRUD для задач команды
+class TeamTableViewSet(viewsets.ModelViewSet):
+    queryset = TeamTable.objects.all()
+    serializer_class = TeamTableSerializer
+    permission_classes = [permissions.IsAuthenticated]
